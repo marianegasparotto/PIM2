6e632766c046e4b2e64b81946d113fdefdd46ae6
marianegasparotto <marigasparotto141@hotmail.com>
2023-01-22 03:56:52 -0300

    finalizei botão ajuda e iniciei criação das funções para codificar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,19 @@ const decod = document.getElementById ('decod')
 const base = document.getElementById ('b64')
 const cifra = document.getElementById('cdc')
 const codec = document.getElementById('codec')
-const msgUsuario = document.getElementById('text1')
-const respUsuario = document.getElementById('text2')
+const msgUsuario = document.getElementById('caixamsg')
+const respUsuario = document.getElementById('caixaresposta')
 const inputinc = document.getElementById ('inputinc')
 const inc = document.getElementsByClassName('inc')
 const tipo = document.getElementById('tipo')
-const resultado = ''
+const usuario = ''
 // ====================Botão radio Cod/Descod 
 
 cod.addEventListener("click", function () {
     alert('codificar')
-    codec.innerText = 'Codificar mensagem'              
-    // btns2 = $('#codec').detach()
-                      
+    codec.innerText = 'Codificar mensagem'                                    
   });
 
-  
 
 decod.addEventListener("click", function () {
     alert('decodificar')
@@ -26,7 +23,12 @@ decod.addEventListener("click", function () {
   });
 
 
-// Precisa atrelar as funções! 
+// =============================== Executa as funções 
+// codec.addEventListener('click', function(){
+//     if(tipo.value === 'Cifra de César'){
+//         resultado.innerText = cifraDeCesar();
+//     }
+// })
 
 
 // ================ Botão de incremento 
@@ -37,88 +39,230 @@ tipo.addEventListener ('change', function(){
     $(inc).css({
         'display':'none'
     })
+    // codec.setAttribute('click',base64)
 }
     else{
             $(inc).css({
                 'display':'block'
             })
+            // codec.setAttribute('click',coCifraDeCesar)
+
         }
     })
-
+// ============================= Botao codec 
+codec.addEventListener("click", function(e){
+    e.preventDefault()
+    if(inputinc.value === 'Cifra de César' && cod)
+    {
+        respUsuario.innerText = coCifraDeCesar()
+    }
+})
 
 // =============================== informativo 
-
-function inserirImagem(){  
+// ------- Abrir imagem ---------
+function inserirImagem(){ 
+    $('.container').css ({
+        'display':'none'    
+    })
+    $('#close').css ({
+        'visibility' : 'visible' 
+    })
     let img = document.createElement('img')
     img.src = './assets/img/img4.png'
     document.body.appendChild(img);
     img.setAttribute('id','info')
+
     }
-
-    // const body = document.getElementsByTagName('body')
-    // body.setAttribute('id','body')
-    // $(html).css{
-    //     'display'= 'none'
-    // }
-
+// ----------------- Fechar imagem -------------- 
+function fecharImagem(){
+        $('.container').css ({
+            'display':'flex'
+        })
+        $('#close').css ({
+            'visibility' : 'hidden',
+            'display':'block'
+        })
+        $('#info').remove()
+    }
 
 // ---------------Codifica em Base64 =========================
 
 
-// function base64(){
-//     let mensagem = msgUsuario.value
-//     if(cod.checked){
-//         let resultCod = msgUsuario.value = BtoA(msgUsuario)
-//         return resultCod 
-//     }
-//     else(decod.checked)
-//     {
-//         let resultDecod = msgUsuario.value = AtoB(msgUsuario)
-//         return resultDecod 
-//     }
-//     respUsuario.innerHTML = resultado
-// }
-
-//     console.log(base64());
-  
-
-
-// =============Decodifica em Base64
-// var encodedStringAtoB = 'SGVsbG8gV29ybGQh';
-// var decodedStringAtoB = atob(encodedStringAtoB);
-
-// console.log(decodedStringAtoB);
-
-
-
-
-
-
-// ======================= Form 
-
-
-    // function codifica(){
-    //     respUsuario.value = msgUsuario
-    // }
+function base64(){
+    let mensagem = msgUsuario.value
+    if(cod.checked){
+        let resultCod = btoa(mensagem)
+        return resultCod
+    }
+    else(decod.checked)
+    {
+        let resultDecod =  atob(mensagem)
+        return resultDecod 
+    }
     
-    // codec.addEventListener('click',codifica)
-    // $('msgUsuario').on('click', codifica)
-    
-    
-   
-    
-    // codec.addEventListener('click', (e) => {
-    //     var seleciona = e.target.value
-    //     if(seleciona === 'base64'){
-    //         codec.setAttribute('onclick','button64()')
-    //     }
-    //     else{
-    //         // codec.setAttribute('onclick','buttoncifra()')
-    //     }
-    // })
+}
+
 
 
 //==Cifra de Cesar====================================================
+
+function coCifraDeCesar (usuario,key){
+    const frase = msgUsuario.value 
+    const coCifraDeCesar = (usuario,key)
+    
+    if(cod.checked){
+    for(var i = 0; i < usuario.length; i++){
+    const letra = usuario.charCodeAt(i)
+    
+    if(frase == ' '){
+        resultado = resultado + ' '
+       
+    }
+    if (letra >= 65 && letra <= 90)
+        {
+            letra = 65 + (letra - 65 + key) % 26
+        }
+    else if(letra >= 97 && letra <= 122)
+        {
+            letra = 97 + (letra - 97 + key) % 26
+
+        }
+        coCifraDeCesar += String.fromCharCode(letra)
+    }
+    return coCifraDeCesar 
+    }
+}
+
+function deCifraDeCesar (usuario,key){
+    const frase = msgUsuario.value 
+    const deCifraDeCesar = (usuario,key)
+    
+    if(cod.checked){
+    for(var i = 0; i < usuario.length; i++){
+    const letra = usuario.charCodeAt(i)
+        
+    if(frase == ' '){
+        resultado = resultado + ' '
+       
+    }
+    if (letra >= 65 && letra <= 90)
+        {
+            letra = 65 + (letra - 65 - key + 26) % 26
+        }
+    else if(letra >= 97 && letra <= 122)
+        {
+            letra = 97 + (letra - 97 - key + 26) % 26
+    
+        }
+        deCifraDeCesar += String.fromCharCode(letra)
+    }
+    return deCifraDeCesar 
+    }    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function cifraDeCesar (usuario,key){
+// const usuario = ''
+// const frase = msgUsuario.value 
+// const valorinc = inputinc.value
+
+// if(cod.checked){
+// for(var i = 0; i < usuario.length; i++){
+// const letra = usuario.charCodeAt(i)
+
+// if(frase == ' '){
+//     resultado = resultado + ' '
+   
+// }
+// if (letra >= 65 && letra <= 90)
+//     {
+//         letra -= key
+//         resultado += String.fromCharCode((letra.charCodeAt(i) + valorinc - 97)% 26 + 65)
+//     }
+// else if(letra >=97 && letra <= 122)
+//     {
+//         letra -= key
+//         resultado += String.fromCharCode((letra.charCodeAt(i) + valorinc - 97 )% 26 + 97)
+//     }
+//     resultado += String.fromCharCode(frase)
+
+// }
+
+// }
+// return resultado
+// // respUsuario.innerHTML = resultado
+
+// =============================Cifra de Cesar 
+
+
+
+
+
+// $('#codec').on('click',codifica);
+
+// // resultado.val(mensagem)
+// // console.log(mensagem)
+
+// function codifica (){
+//     // var mensagem = document.querySelector("#caixamsg").value
+//     // var resultado = document.querySelector("#caixaresposta")
+//     $('#caixamsg').val(
+//     $('#caixaresposta')
+
+// )
+//     resultado.value = mensagem
+// }
+
+// document.getElementById('codec').addEventListener('click',codifica)
+
+
+// codec.addEventListener("click", function(){
+//     msgUsuario.value = respUsuario.value
+//     msgUsuario.value = ""
+// })
+
+
+
+
+// "A" .charCodeAt(0)
+
+// (codigoDaLetra + desloc) % tamDoAlfabeto + 65
+//                      65                       26             65
+// ((códigoDaLetra - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+
+
+// }
+
 // const decodeCC = (phrase, key) => {
 // 	let decodedPhrase = '';
 // 	for (let i = 0; i < phrase.length; i++) {
@@ -148,19 +292,6 @@ function inserirImagem(){
 // };
 
 
-// =============================Cifra de Cesar 
-// "A" .charCodeAt(0)
-
-// (codigoDaLetra + desloc) % tamDoAlfabeto + 65
-//                      65                       26             65
-// ((códigoDaLetra - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
-
-// A - Z = 65 á 90
-// a - z = 97 á 122
-// function cdcesar(){
-
-// }
-
 // ==================================================================
 
 // var input = msgUsuario.value
@@ -175,7 +306,3 @@ function inserirImagem(){
 
 // var decodedStringBtoA = 'Hello World!'
 // var encodedStringBtoA = btoa(decodedStringBtoA);
-
-
-
-
